03951aa3b9423e32d36021c11cccc79011d7f8d8
Your Name <youremail@example.com>
2019-09-04 21:42:38 +0800

    封装axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import './utils/request'
 
 Vue.config.productionTip = false
-
+Vue.use(Vant)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
---
+++ src/utils/request.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import JSONbig from 'json-bigint'
+
+// 创建一个axios实例,设置不同的baseURL
+const instance = axios.create({
+    timeout:1000,
+    baseURL:'http://ttapi.research.itcast.cn'
+}) 
+
+// 获取服务器返回的数据，并且是在处理数据之前使用
+// transformResponse 是在响应拦截器之前执行
+request.defaults.transformResponse = [function (data){
+try{
+    //data数据可能不是标准的JSON格式字符串，否则会导致JSONbig.parse(data)
+    data.JSONbig.parse(data)
+}catch(err){
+    //无法转换的数据直接原样返回
+    return data
+    }
+}]
+// 请求拦截器
+instance.interceptors.request.use(function (config) {
+    // console.log(config)
+    return config
+},function (error) {
+    return Promise.reject(error)
+})
+
+// 响应拦截器
+instance.interceptors.response.use(function(response){
+    return response
+},function(error){
+    return Promise.reject(error)
+})
+
+export default instance
